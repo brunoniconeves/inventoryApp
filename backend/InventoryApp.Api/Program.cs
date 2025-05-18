4e4aa63141540761a126e0b4950135e774101b52
Bruno Bastos Neves <brunoniconeves@gmail.com>
2025-05-17 22:55:08 -0300

    remove minimal API template code

--- backend/InventoryApp.Api/Program.cs
+++ backend/InventoryApp.Api/Program.cs
@@ -108,8 +108,3 @@ public partial class Program
         app.Run();
     }
 }
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
